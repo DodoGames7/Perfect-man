ef8d0e09d3e30aa7793a166e61a3b9de74b0b7e4
Glitch (glitch-hello-node) <none>
2022-05-12 22:47:50 +0000

    📖😺 Checkpoint ./Commands/fun/delete.js:26877849/109

--- Commands/fun/rip.js
+++ Commands/fun/rip.js
@@ -1,19 +1,20 @@
-const { Discord } = require('discord.js')
+const { MessageAttachment } = require('discord.js')
 
 const DIG = require("discord-image-generation");
 
 module.exports = {
-  name: "delete",
-  description: "posts the delete image meme",
+  name: "rip",
+  description: "posts the press f to respect image meme",
     aliases: [""],
   permissions: [],
   cooldown: 3000,
   run: async (client, message, args) => {
     let avatar = message.author.displayAvatarURL({ dynamic: false, format: 'png' });
     
-    let img = await new DIG.Delete().getImage(avatar)
+    let img = await new DIG.Rip().getImage(avatar)
     
-    let attach = new Discord.MessageAttachment(img, "delete.png");
+    let attach = new MessageAttachment(img, 'rip.png')
+    
     return message.channel.send({ files: [attach] })
   }
 }
