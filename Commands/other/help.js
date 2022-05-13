47b09fb51951752e9d8ef98e10a183cd96cc86f5
Glitch (glitch-hello-node) <none>
2022-05-13 01:46:59 +0000

    🎤💧 Checkpoint ./Commands/other/help.js:26877849/1214

--- Commands/other/help.js
+++ Commands/other/help.js
@@ -9,3 +9,42 @@ module.exports = {
   permissions: [],
   cooldown: 3000,
   run: async (client, message, args) => {
+const page1 = new MessageEmbed()
+            .setTitle("fun Commands")
+            .setFooter('Page 1/3')
+            .setColor('RANDOM')
+            .setTimestamp()
+
+        const page2 = new MessageEmbed()
+            .setTitle("Other Commands")
+            .setFooter('Page 2/3')
+            .setColor('RANDOM')
+            .setTimestamp()
+
+        const page3 = new MessageEmbed()
+            .setTitle("Utils Commands")
+            .setFooter('Page 3/3')
+            .setColor('RANDOM')
+            .setTimestamp()
+
+        const pages = [page1, page2, page3]
+
+        await ButtonPages({
+            message: message,
+            slash_command: false,
+            embeds: pages,
+            time: 300000, // how long before buttons get disabled in ms
+            back: {
+                label: ' Prev',
+                style: 'PRIMARY',
+                emoji: '⬅️',
+            },
+            next: {
+                label: ' Next',
+                style: 'PRIMARY',
+                emoji: '➡️',
+            },
+            authorOnly: 'Only <@{{author}}> can use these buttons!',
+        })
+  }
+}
