f7d92769656fd02d96481be6c14c08ace5332a97
Glitch (glitch-hello-node) <none>
2022-05-13 01:22:59 +0000

    🌥🌊 Checkpoint ./Commands/other/stats.js:26877849/25

--- Commands/other/stats.js
+++ Commands/other/stats.js
@@ -10,9 +10,9 @@ module.exports = {
   run: async (client, message, args) => {
     const statsembed = new MessageEmbed()
     .setTitle(`${client.user.username}'s stats`)
-    .addField('Discord.js', `\`v${version}`, true)
+    .addField('Discord.js', `\`v${version}\``, true)
     .addField('platform', `${os.platform}`, true)
-    .addField('Node', `${process.version}`, true)
+    .addField('Node.js', `\`${process.version}\``, true)
     .addField('server Count', `${client.guilds.cache.size}`, true)
     .addField('user Count', `${client.guilds.cache.map((g) => g.memberCount || 0).reduce((x, y) => x + y, 0)}`, true)
     .setColor('RANDOM');
